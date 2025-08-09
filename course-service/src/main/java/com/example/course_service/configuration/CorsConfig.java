c1d89a31a4bebbb5e170e11c21fcf94ca4dfdd2a
akoubaa <ahmed.koubaa@kleegroup.com>
2025-08-09 14:22:15 +0200

    met a jour .env

--- course-service/src/main/java/com/example/course_service/configuration/CorsConfig.java
+++ course-service/src/main/java/com/example/course_service/configuration/CorsConfig.java
@@ -1,11 +1,13 @@
 package com.example.course_service.configuration;
 
+import io.micrometer.common.lang.NonNullApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@NonNullApi
 public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -13,7 +15,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",  // dev local Vite
+                                "http://localhost:3000",  // prod Docker accessible depuis le PC
+                                "http://frontend:80"      // appel interne Docker
+                        )
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }

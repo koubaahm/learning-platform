59da0f582eab947480d396e0b3afabe1c1a4c222
akoubaa <ahmed.koubaa@kleegroup.com>
2025-08-10 23:44:26 +0200

    fix docker-compose probleme elasticsearch

--- course-service/src/main/java/com/example/course_service/configuration/ElasticsearchConfig.java
+++ course-service/src/main/java/com/example/course_service/configuration/ElasticsearchConfig.java
@@ -1,9 +1,20 @@
 package com.example.course_service.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.example.course_service.search.repository")
 public class ElasticsearchConfig {
+    @Value("${spring.elasticsearch.uris}")
+    private String uris;
+
+
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(uris.replace("http://","").replace("https://",""))
+                .build();
+    }
 }

9d4313f54a408f6ca2120b895b3d09e2665fd0a0
akoubaa <ahmed.koubaa@kleegroup.com>
2025-08-10 03:16:55 +0200

    fix(migration): correction migration

--- course-service/src/main/resources/db/migration/V1__create_course_table.sql
+++ course-service/src/main/resources/db/migration/V1__create_course_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE course (
                         course_name VARCHAR(255) NOT NULL,
                         course_description TEXT,
                         category VARCHAR(100),
-                        instructor VARCHAR(100)
+                        instructor VARCHAR(100),
+                        created_date TIMESTAMP
 );
